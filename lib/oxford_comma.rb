a57bc9a536a38bf3bbf929f613892e2ee74f0364
Will Jones <willjones2010@gmail.com>
2019-03-22 03:49:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,9 @@ def oxford_comma(array)
   elsif array.length == 2
    array.join(" and ")
   else array.length >= 3
-    new_item = ", and #{array."
-      array.join(", and")
-    else
-   array.join(", ")
+    new_item = ", and #{array[-1]}"
+      array.push(new_item)
+    array.join(", ")
  end
 end
 end
