f14fbd521f90f6d1bf650af27a6864595518152c
Will Jones <willjones2010@gmail.com>
2019-03-22 03:50:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
    array.join(" and ")
   else array.length >= 3
     new_item = "and #{array[-1]}"
-      array.push(new_item)
+      array.pop(new_item)
     array.join(", ")
  end
 end
